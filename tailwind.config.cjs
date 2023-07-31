9ba75483a9594b169b86401b24a6a1f80bb364d6
Diego Sánchez <diegosandmg@gmail.com>
2023-07-30 21:28:51 -0400

    flowbite installation
    With an example of the modal component

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  // If any css in js has to be added, use
+  // "./static/src/**/*.js"
+  content: [
+    "./templates/**/*.html",
+    // Include Flowbite
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
