5ae5fa865f53a1d1591b070cb9db17b0ea46bc7a
Diego Sánchez <diegosandmg@gmail.com>
2023-10-12 21:55:46 -0400

    feat: alpine js integration and profile_selector component

---
+++ static/src/js/components/profile_selector.ts
@@ -0,0 +1,30 @@
+import Alpine from "alpinejs"
+
+// Global store for instance comunication
+Alpine.store('profile', {
+    profile: "",
+    change(prfl: string){
+        this.profile = prfl
+    }
+})
+
+// Component definition
+const profile_selector = (_profile: string) => ({
+    profile: _profile,
+
+    /* Computed property to know if the the current component is selected */
+    get selected() {
+        return Alpine.store("profile").profile == this.profile
+    },
+    /* onclic event handler, sets the store profile */
+    set_current() {
+        Alpine.store("profile").change(this.profile)
+        console.log(Alpine.store("profile").profile)
+    }
+})
+
+
+// Register component
+Alpine.data('profile_selector', profile_selector)
+
+Alpine.start()
