5393d579e58c52535a8ce7eabb649b5587b40d9c
Diego Sánchez <diegosandmg@gmail.com>
2023-10-15 13:02:17 -0400

    fix order of collec static instruction
    Order of collec static instruction was before downloading the new docker image

--- shscripts/vps_update.sh
+++ shscripts/vps_update.sh
@@ -5,14 +5,14 @@ source ~/.zprofile
 
 sudo --preserve-env docker compose stop && \
 
+# Update docker image for django service
+sudo --preserve-env docker pull --quiet "$DOCKER_IMAGE" &> /dev/null && \
+
 # Force update static files in the django_static_files volume
 # otherwise it will use old stuff
 sudo --preserve-env docker compose run --rm django python manage.py collectstatic \
   --settings django_src.settings.production \
   --noinput
-
-# Update docker image for django service
-sudo --preserve-env docker pull --quiet "$DOCKER_IMAGE" &> /dev/null && \
 
 # Start again everything
 sudo --preserve-env docker compose up -d
