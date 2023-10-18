adf2ac046586ffb82177eb8b193ac99cba51b7f0
Diego Sánchez <diegosandmg@gmail.com>
2023-10-17 21:06:09 -0400

    initial version of the career selector component

--- django_src/apps/register/urls.py
+++ django_src/apps/register/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import MainView
+from .views import MainView, SelectCarreraView
 
 app_name="register"
 
 urlpatterns = [
-    path("", MainView.as_view(), name="index")
+    path("select_carrera/", SelectCarreraView.as_view(), name="select_carrera"),
+    path("", MainView.as_view(), name="index"),
+
 ]
--- django_src/apps/register/views.py
+++ django_src/apps/register/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.urls import reverse
 
 # Create your views here.
 class MainView(TemplateView):
     template_name = "register/main.html"
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+
+        select_carrera_url = reverse("register:select_carrera")
+        context["form_urls"] = {
+            "estudiante": select_carrera_url,
+            "mentor": select_carrera_url,
+            "empresa": "todo",
+        }
+        return context
+
+class SelectCarreraView(TemplateView):
+    template_name = "register/select_carrera.html"
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        context["step_urls"] = {
+            "select_perfil": reverse("register:index")
+        }
+        return context
