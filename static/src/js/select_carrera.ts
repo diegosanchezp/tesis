67f9caa4ba0d0ca335a46291bb467b1de6a9619d
Diego Sánchez <diegosandmg@gmail.com>
2023-11-07 16:47:02 -0400

    remove not needed event handler

--- static/src/js/select_carrera.ts
+++ static/src/js/select_carrera.ts
@@ -6,13 +6,6 @@ const select_carrera_def = () => ({
     carreer: Alpine.$persist('').as('carreer'),
     selected: false,
 
-    handle_selected(carrer_name: string){
-        return () => {
-            this.carreer = carrer_name;
-            this.selected = true;
-        }
-    },
-
     get next_url(){
         return `/register/select_carreer_specialization/${this.carreer}`
     }
