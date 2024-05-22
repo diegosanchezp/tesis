c75ace043cf27259553041cfcfd1792493f89b9d
Diego Sánchez <diegosandmg@gmail.com>
2024-05-21 20:20:28 -0400

    feat: add view permissions to approvals view and menu

--- django_src/apps/register/wagtail_hooks.py
+++ django_src/apps/register/wagtail_hooks.py
@@ -10,6 +10,17 @@ def register_admin_urls():
         path('approvals/', approvals_view, name='approvals'),
     ]
 
+menu_aprobaciones = MenuItem(label='Aprobaciones', url=reverse_lazy('approvals'))
+
 @hooks.register('register_admin_menu_item')
 def register_calendar_menu_item():
-    return MenuItem('Aprobaciones', reverse_lazy('approvals'))
+    return menu_aprobaciones
+
+@hooks.register('construct_main_menu')
+def hide_approvals_for_mentors(request, menu_items):
+    """
+    Approvals menu should not be shown to mentors
+    """
+    # Delete the Approvals MenuItem from the menu_items list, if the user is a mentor
+    if request.user.is_mentor:
+        menu_items[:] = [item for item in menu_items if item.label != menu_aprobaciones.label]
