c8157eb950a6bf1c26cc08f5b4bbe14156394768
Diego Sánchez <diegosandmg@gmail.com>
2024-09-30 16:28:25 -0400

    Add atomic transactions to all dev fixtures upload_data
    - Fix: interest_themes_data dependencies integrity errors
    - Fix: ADMIN_USERNAME, ADMIN_PASSWORD having nesteed double quoutes when being read

--- django_src/apps/register/upload_data.py
+++ django_src/apps/register/upload_data.py
@@ -24,128 +24,128 @@ def create_interest_themes(carreer, interest_themes_data: InterestThemeData):
     carreer.interest_themes.add(*interest_themes_data.get_all_interests())
 
 
+@transaction.atomic
 def create_carreers(interest_themes_data: InterestThemeData):
-    with transaction.atomic():
-        Faculty = apps.get_model(app_label, "Faculty")
+    Faculty = apps.get_model(app_label, "Faculty")
 
-        agronomia = Faculty.objects.create(
-            name="Agronomía",
+    agronomia = Faculty.objects.create(
+        name="Agronomía",
+    )
+
+    agronomia.carreers.create(name="Agronomía")
+
+    arquitectura = Faculty.objects.create(
+        name="Arquitectura y Urbanismo",
+    )
+
+    arquitectura.carreers.create(name="Arquitectura")
+
+    ciencias = Faculty.objects.create(
+        name="Ciencias",
+    )
+
+    geoquimica = ciencias.carreers.create(name="Geoquímica")
+    matematica = ciencias.carreers.create(name="Matemática")
+    quimica = ciencias.carreers.create(name="Química")
+    biologia = ciencias.carreers.create(name="Biología")
+    computacion = ciencias.carreers.create(name="Computación")
+
+    computacion_specialization = [
+        "Aplicaciones Tecnología Internet",
+        "Inteligencia artificial",
+        "Base de datos",
+        "Ingeniería de Software",
+        "Computación Gráfica",
+        "Sistemas de información",
+        "Sistemas distribuidos y paralelos",
+        "Calculo Científico",
+        "Modelos matemáticos",
+        "Tecnologías educativas",
+        "Tecnologías en Comunicaciones y Redes de Computadoras",
+    ]
+
+    for carrerspecialization in computacion_specialization:
+        computacion.carrerspecialization_set.create(
+            name=carrerspecialization,
         )
 
-        agronomia.carreers.create(name="Agronomía")
+    create_interest_themes(computacion, interest_themes_data)
 
-        arquitectura = Faculty.objects.create(
-            name="Arquitectura y Urbanismo",
-        )
+    ciencias.carreers.create(name="Física")
 
-        arquitectura.carreers.create(name="Arquitectura")
+    faces = Faculty.objects.create(
+        name="Ciencias Económicas y Sociales",
+    )
+    faces.carreers.create(name="Administración y Contaduría")
+    faces.carreers.create(name="Antropología")
+    faces.carreers.create(name="Estadística y Ciencias Actuariales")
+    faces.carreers.create(name="Economía")
+    faces.carreers.create(name="Estudios Internacionales")
+    faces.carreers.create(name="Sociología")
+    faces.carreers.create(name="Trabajo Social")
 
-        ciencias = Faculty.objects.create(
-            name="Ciencias",
-        )
+    derecho = Faculty.objects.create(
+        name="Ciencias Jurídicas y Políticas",
+    )
 
-        geoquimica = ciencias.carreers.create(name="Geoquímica")
-        matematica = ciencias.carreers.create(name="Matemática")
-        quimica = ciencias.carreers.create(name="Química")
-        biologia = ciencias.carreers.create(name="Biología")
-        computacion = ciencias.carreers.create(name="Computación")
+    veterinaria = Faculty.objects.create(
+        name="Ciencias Veterinarias",
+    )
 
-        computacion_specialization = [
-            "Aplicaciones Tecnología Internet",
-            "Inteligencia artificial",
-            "Base de datos",
-            "Ingeniería de Software",
-            "Computación Gráfica",
-            "Sistemas de información",
-            "Sistemas distribuidos y paralelos",
-            "Calculo Científico",
-            "Modelos matemáticos",
-            "Tecnologías educativas",
-            "Tecnologías en Comunicaciones y Redes de Computadoras",
-        ]
+    farmacia = Faculty.objects.create(
+        name="Farmacia",
+    )
 
-        for carrerspecialization in computacion_specialization:
-            computacion.carrerspecialization_set.create(
-                name=carrerspecialization,
-            )
+    humanidades = Faculty.objects.create(
+        name="Humanidades y Educación",
+    )
 
-        create_interest_themes(computacion, interest_themes_data)
+    humanidades.carreers.create(name="Artes")
+    humanidades.carreers.create(name="Bibliotecología y Archivología")
+    humanidades.carreers.create(name="Comunicación Social")
+    humanidades.carreers.create(name="Educación")
+    humanidades.carreers.create(name="Filosofía")
+    humanidades.carreers.create(name="Geografía")
+    humanidades.carreers.create(name="Historia")
+    humanidades.carreers.create(name="Idiomas Modernos")
+    humanidades.carreers.create(name="Letras")
+    humanidades.carreers.create(name="Psicología")
 
-        ciencias.carreers.create(name="Física")
+    ingenieria = Faculty.objects.create(
+        name="Ingeniería",
+    )
 
-        faces = Faculty.objects.create(
-            name="Ciencias Económicas y Sociales",
-        )
-        faces.carreers.create(name="Administración y Contaduría")
-        faces.carreers.create(name="Antropología")
-        faces.carreers.create(name="Estadística y Ciencias Actuariales")
-        faces.carreers.create(name="Economía")
-        faces.carreers.create(name="Estudios Internacionales")
-        faces.carreers.create(name="Sociología")
-        faces.carreers.create(name="Trabajo Social")
+    medicina = Faculty.objects.create(
+        name="Medicina",
+    )
 
-        derecho = Faculty.objects.create(
-            name="Ciencias Jurídicas y Políticas",
-        )
+    odontologia = Faculty.objects.create(
+        name="Odontología",
+    )
 
-        veterinaria = Faculty.objects.create(
-            name="Ciencias Veterinarias",
-        )
+    from django_src.apps.register.models import Carreer, Faculty
 
-        farmacia = Faculty.objects.create(
-            name="Farmacia",
-        )
+    @dataclass
+    class ModelList:
+        computacion: Carreer
+        geoquimica: Carreer
+        matematica: Carreer
+        quimica: Carreer
+        biologia: Carreer
+        ingenieria: Faculty
+        medicina: Faculty
+        odontologia: Faculty
 
-        humanidades = Faculty.objects.create(
-            name="Humanidades y Educación",
-        )
-
-        humanidades.carreers.create(name="Artes")
-        humanidades.carreers.create(name="Bibliotecología y Archivología")
-        humanidades.carreers.create(name="Comunicación Social")
-        humanidades.carreers.create(name="Educación")
-        humanidades.carreers.create(name="Filosofía")
-        humanidades.carreers.create(name="Geografía")
-        humanidades.carreers.create(name="Historia")
-        humanidades.carreers.create(name="Idiomas Modernos")
-        humanidades.carreers.create(name="Letras")
-        humanidades.carreers.create(name="Psicología")
-
-        ingenieria = Faculty.objects.create(
-            name="Ingeniería",
-        )
-
-        medicina = Faculty.objects.create(
-            name="Medicina",
-        )
-
-        odontologia = Faculty.objects.create(
-            name="Odontología",
-        )
-
-        from django_src.apps.register.models import Carreer, Faculty
-
-        @dataclass
-        class ModelList:
-            computacion: Carreer
-            geoquimica: Carreer
-            matematica: Carreer
-            quimica: Carreer
-            biologia: Carreer
-            ingenieria: Faculty
-            medicina: Faculty
-            odontologia: Faculty
-
-        return ModelList(
-            geoquimica=geoquimica,
-            matematica=matematica,
-            quimica=quimica,
-            biologia=biologia,
-            computacion=computacion,
-            ingenieria=ingenieria,
-            medicina=medicina,
-            odontologia=odontologia,
-        )
+    return ModelList(
+        geoquimica=geoquimica,
+        matematica=matematica,
+        quimica=quimica,
+        biologia=biologia,
+        computacion=computacion,
+        ingenieria=ingenieria,
+        medicina=medicina,
+        odontologia=odontologia,
+    )
 
 
 def upload_data():
